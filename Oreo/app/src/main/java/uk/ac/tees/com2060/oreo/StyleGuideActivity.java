dafa97ecab19edb51f87ed3cd1a78276e8c9c293
Tom Wilson <tom@iwm.pw>
2018-02-12 20:36:03 +0000

    EP-39 Styled spinners, and moved activity to "style guide"

---
+++ Oreo/app/src/main/java/uk/ac/tees/com2060/oreo/StyleGuideActivity.java
@@ -0,0 +1,35 @@
+package uk.ac.tees.com2060.oreo;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class StyleGuideActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        // Part of the fix for Google issue #63250768 relating to custom fonts for switches
+        DataBindingUtil.setContentView(this, R.layout.activity_welcome);
+
+        Spinner spinner = findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.demo_spinner_day, R.layout.spinner_rounded_layout);
+        adapter.setDropDownViewResource(R.layout.spinner_rounded_layout_item);
+        spinner.setAdapter(adapter);
+
+        Spinner spinner2 = findViewById(R.id.spinner3);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.demo_spinner_month, R.layout.spinner_rounded_layout);
+        adapter2.setDropDownViewResource(R.layout.spinner_rounded_layout_item);
+        spinner2.setAdapter(adapter2);
+
+        Spinner spinner3 = findViewById(R.id.spinner4);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.demo_spinner_year, R.layout.spinner_rounded_layout);
+        adapter3.setDropDownViewResource(R.layout.spinner_rounded_layout_item);
+        spinner3.setAdapter(adapter3);
+    }
+}
