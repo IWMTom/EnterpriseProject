c9868de38093c9b64eeda95512de8fc3786fdfb8
Tom Wilson <tom@iwm.pw>
2018-02-15 23:22:25 +0000

    EP-40 finished step 1 registration
    also updated logo

--- Oreo/app/src/main/java/uk/ac/tees/com2060/oreo/RegisterStep1Fragment.java
+++ Oreo/app/src/main/java/uk/ac/tees/com2060/oreo/RegisterStep1Fragment.java
@@ -2,12 +2,16 @@ package uk.ac.tees.com2060.oreo;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,6 +19,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -24,14 +29,121 @@ import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 import static android.app.Activity.RESULT_OK;
 
 public class RegisterStep1Fragment extends Fragment
 {
     RegisterStep1Listener mCallback;
+    Bitmap profile_photo;
+    EditText editText_full_name;
+    EditText editText_known_as;
+    EditText editText_postcode;
+    EditText editText_dob;
+    Button button_next_step;
 
+    /**
+     * Required empty constructor
+     */
     public RegisterStep1Fragment() {}
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_register_step1, container, false);
+        setHasOptionsMenu(true);
+
+        editText_dob = (EditText) view.findViewById(R.id.editText_dob);
+        editText_full_name = (EditText) view.findViewById(R.id.editText_full_name);
+        editText_known_as = (EditText) view.findViewById(R.id.editText_known_as);
+        editText_postcode = (EditText) view.findViewById(R.id.editText_postcode);
+        button_next_step = (Button) view.findViewById(R.id.button_next_step);
+
+        editText_full_name.addTextChangedListener(watcher);
+        editText_known_as.addTextChangedListener(watcher);
+        editText_postcode.addTextChangedListener(watcher);
+        editText_dob.addTextChangedListener(watcher);
+
+        /**
+         * Listens for when the date picker dialog has been used, and sets the text
+         * in the date of birth field to be the selected date
+         */
+        final Calendar c = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2)
+            {
+                c.set(Calendar.YEAR, i);
+                c.set(Calendar.MONTH, i1);
+                c.set(Calendar.DAY_OF_MONTH, i2);
+
+                SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy", Locale.UK);
+                editText_dob.setText(df.format(c.getTime()));
+            }
+        };
+
+        /**
+         * onClick listener for the date of birth field, which opens the date picker dialog
+         */
+        editText_dob.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                DatePickerDialog dialog = new DatePickerDialog(view.getContext(), date,
+                        c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
+
+                dialog.show();
+            }
+        });
+
+
+        /**
+         * onClick listener for the next step button
+         * If a profile photo hasn't been set, a warning message will be displayed to give the
+         * user the opportunity to set one before continuing. If they choose to continue, the
+         * fields will be validated and step 2 will be displayed
+         */
+        button_next_step.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage(R.string.activity_register_step1_no_profile_photo)
+                    .setPositiveButton(R.string.activity_register_step1_no_profile_photo_no,
+                            new DialogInterface.OnClickListener()
+                    {
+                        public void onClick(DialogInterface dialog, int id)
+                        {
+                            if (validateFields())
+                            {
+                                mCallback.step1Listener();
+                            }
+                        }
+                    })
+                    .setNegativeButton(R.string.activity_register_step1_no_profile_photo_yes, null);
+                builder.create();
+                builder.show();
+            }
+        });
+
+        return view;
+    }
+
+    public boolean validateFields()
+    {
+        if (!editText_postcode.getText().toString().matches(
+                "^[a-zA-Z]{1,2}([0-9]{1,2}|[0-9][a-zA-Z])\\s*[0-9][a-zA-Z]{2}$"))
+        {
+            Utils.displayMessage(getActivity(), R.string.validation_invalid_postcode, R.string.okay);
+            return false;
+        }
+
+        return true;
+    }
 
     public interface RegisterStep1Listener
     {
@@ -54,50 +166,40 @@ public class RegisterStep1Fragment extends Fragment
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    /**
+     * Watches all text fields in the fragment and tests whether they're all filled in
+     * When all the fields are completed, the next step button is activated.
+     */
+    private TextWatcher watcher = new TextWatcher()
     {
-        View view = inflater.inflate(R.layout.fragment_register_step1, container, false);
-        setHasOptionsMenu(true);
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
-//        Button nextStep = (Button) view.findViewById(R.id.button9);
-//        nextStep.setOnClickListener(new View.OnClickListener()
-//        {
-//            public void onClick(View v)
-//            {
-//                mCallback.step1Listener();
-//            }
-//        });
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
-
-        final Calendar c = Calendar.getInstance();
-        final EditText dobField = (EditText) view.findViewById(R.id.editText_dob);
-        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener()
+        @Override
+        public void afterTextChanged(Editable editable)
         {
-            @Override
-            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-                c.set(Calendar.YEAR, i);
-                c.set(Calendar.MONTH, i1);
-                c.set(Calendar.DAY_OF_MONTH, i2);
-
-                SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy", Locale.UK);
-                dobField.setText(df.format(c.getTime()));
-            }
-        };
-
-        dobField.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
+            if (editText_full_name.getText().toString().length() != 0
+                    && editText_known_as.getText().toString().length() != 0
+                    && editText_postcode.getText().toString().length() != 0
+                    && editText_dob.getText().toString().length() != 0)
             {
-                DatePickerDialog dialog = new DatePickerDialog(view.getContext(), date, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
-                dialog.show();
+                // Enable button, set style to primary
+                button_next_step.setEnabled(true);
+                button_next_step.setBackgroundResource(R.drawable.button_rounded_primary);
+                button_next_step.setTextColor(getResources().getColor(R.color.colorWhite));
             }
-        });
-
-
-        return view;
-    }
+            else
+            {
+                // Disable button, set style to disabled
+                button_next_step.setEnabled(false);
+                button_next_step.setBackgroundResource(R.drawable.button_rounded_disabled);
+                button_next_step.setTextColor(getResources().getColor(R.color.colorDarkGrey));
+            }
+        }
+    };
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
@@ -120,6 +222,12 @@ public class RegisterStep1Fragment extends Fragment
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Used for the profile picture image picker functionality
+     * @param requestCode
+     * @param resultCode
+     * @param imageReturnedIntent
+     */
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent)
     {
@@ -131,10 +239,10 @@ public class RegisterStep1Fragment extends Fragment
             {
                 final Uri imageUri = imageReturnedIntent.getData();
                 final InputStream imageStream = getActivity().getContentResolver().openInputStream(imageUri);
-                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                profile_photo = BitmapFactory.decodeStream(imageStream);
 
                 ImageView profilePhoto = (ImageView) getView().findViewById(R.id.imageView_profile_photo);
-                profilePhoto.setImageBitmap(selectedImage);
+                profilePhoto.setImageBitmap(profile_photo);
             }
             catch (FileNotFoundException e)
             {
--- Oreo/app/src/main/java/uk/ac/tees/com2060/oreo/Utils.java
+++ Oreo/app/src/main/java/uk/ac/tees/com2060/oreo/Utils.java
@@ -1,8 +1,11 @@
 package uk.ac.tees.com2060.oreo;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.databinding.BindingAdapter;
 import android.support.annotation.FontRes;
 import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AlertDialog;
 import android.widget.Switch;
 
 public class Utils
@@ -12,5 +15,14 @@ public class Utils
     public static void setLabelTypeface(Switch view, @FontRes int id)
     {
         view.setTypeface(ResourcesCompat.getFont(view.getContext(), id));
+    }
+
+    public static void displayMessage(Activity activity, int message_resource, int button_resource)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setMessage(message_resource)
+                .setPositiveButton(button_resource, null);
+        builder.create();
+        builder.show();
     }
 }
